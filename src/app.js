86e48adddbfdcbe646ee730187c8f6c0540137b9
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-11-29 22:09:58 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let button = document.querySelector("#cardGenerator");
+  let cardValueList = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let cardTypeList = ["♦", "♥", "♠", "♣"];
+
+  let randomCardValue =
+    cardValueList[Math.floor(Math.random() * cardValueList.length)];
+  let randomCardType =
+    cardTypeList[Math.floor(Math.random() * cardTypeList.length)];
+
+  let cardGenerator = event => {
+    let cardType = document.querySelectorAll(".card-type");
+    let cardValue = document.querySelector("#cardValue");
+    let styleTypeUp = cardType[0].classList;
+    let styleTypeDown = cardType[1].classList;
+
+    cardType[0].innerHTML = randomCardType;
+    cardType[1].innerHTML = randomCardType;
+    cardValue.innerHTML = randomCardValue;
+
+    if (randomCardType === "♣" || randomCardType === "♠") {
+      styleTypeUp.remove("text-danger");
+      styleTypeDown.remove("text-danger");
+    }
+  };
+  button.addEventListener("click", cardGenerator);
 };
