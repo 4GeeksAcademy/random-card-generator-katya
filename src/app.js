e7006bc6f58f0c9e3d10b73f854fbf7f303ef6ec
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2023-11-30 17:38:05 +0000

    Optimized cardValue conditional variables

--- src/app.js
+++ src/app.js
@@ -15,37 +15,32 @@ window.onload = function() {
   const cardTypeList = ["♦", "♥", "♠", "♣", "J"];
 
   let cardGenerator = event => {
-
     let randomCardValue =
       cardValueList[Math.floor(Math.random() * cardValueList.length)];
     let randomCardType =
       cardTypeList[Math.floor(Math.random() * cardTypeList.length)];
 
+    cardType[0].innerHTML = randomCardType;
+    cardType[1].innerHTML = randomCardType;
+
+    cardValue.innerHTML =
+      randomCardType === "J"
+        ? "<img src='src/assets/img/istockphoto-538781960-612x612.jpeg' style='height: 150px;' />"
+        : randomCardValue;
+
     let styleTypeUp = cardType[0].classList;
     let styleTypeDown = cardType[1].classList;
 
-    cardType[0].innerHTML = randomCardType;
-    cardType[1].innerHTML = randomCardType;
-    cardValue.innerHTML = randomCardValue;
-
     if (randomCardType === "♣" || randomCardType === "♠") {
-
       styleTypeUp.remove("text-danger");
       styleTypeDown.remove("text-danger");
       styleTypeUp.add("text-black");
       styleTypeDown.add("text-black");
-      
     } else if (randomCardType === "♦" || randomCardType === "♥") {
-
       styleTypeUp.remove("text-black");
       styleTypeDown.remove("text-black");
       styleTypeUp.add("text-danger");
       styleTypeDown.add("text-danger");
-
-    } else if (randomCardType === "J") {
-      
-      cardValue.innerHTML =
-        "<img src='src/assets/img/istockphoto-538781960-612x612.jpeg' style='height: 150px;' />";
     }
   };
   button.addEventListener("click", cardGenerator);
